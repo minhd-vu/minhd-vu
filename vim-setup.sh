e28ffe555f73f04b226c3a4c17013fe1a132e706
Minh Vu <minhvu@minh-ubuntu.lionsden.local>
2020-11-21 13:13:30 -0500

    added usage for vim setup

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,3 +1,4 @@
+# Usage: sh vim-setup.sh
 sudo apt update
 sudo apt install -y vim build-essential cmake vim python3-dev
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
