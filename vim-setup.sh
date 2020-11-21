2769758abe241a703323cf636fc2973c707ec7d3
Minh Vu <minhvu@minh-ubuntu.lionsden.local>
2020-11-21 13:03:45 -0500

    added youcompleteme

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,5 +1,7 @@
 sudo apt update
-sudo apt install vim build-essential cmake vim python3-dev
+sudo apt install -y vim build-essential cmake vim python3-dev
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~/
 vim +'PlugInstall --sync' +qa
+cd ~/.vim/plugged/youcompleteme
+python3 install.py --clangd-completer
