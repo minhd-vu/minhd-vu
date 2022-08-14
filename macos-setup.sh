8339dba491a50c91cdd9d9b6f772147e43e45494
Minh Vu <minhd_vu@yahoo.com>
2022-08-14 12:02:26 -0400

    update macos-setup

--- macos-setup.sh
+++ macos-setup.sh
@@ -3,6 +3,8 @@
 brew install neovim cmake python go nodejs java fzf
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # java
 sudo ln -sfn $(brew --prefix java)/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 # vim-plug
