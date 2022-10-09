1634b105ae1a32a511e931b42bb9a9b7a9160c48
Minh Vu <minhd_vu@yahoo.com>
2022-10-09 17:09:41 -0400

    remove vim

--- macos-setup.sh
+++ macos-setup.sh
@@ -5,12 +5,4 @@ brew install neovim cmake python go nodejs java fzf
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# java
-sudo ln -sfn $(brew --prefix java)/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-# vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-cp ./.zshrc ~/
-cp -p ./init.vim ~/.config/nvim/
-nvim +'PlugInstall --sync' +qa
 
--- vim-setup.sh
+++
@@ -1,8 +0,0 @@
-# Usage: sh vim-setup.sh
-sudo apt update
-sudo apt install -y vim build-essential cmake vim python3-dev
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp ./.vimrc ~/
-vim +'PlugInstall --sync' +qa
-cd ~/.vim/plugged/youcompleteme
-python3 install.py --clangd-completer
