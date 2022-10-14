44ead263a874e4559de8dc57c79ec0ccf72f8588
Minh Vu <minhd_vu@yahoo.com>
2022-10-13 22:42:27 -0400

    remove brewfile

--- macos-setup.sh
+++ macos-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install neovim cmake python go nodejs java fzf
+brew install neovim python go nvm java fzf 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
