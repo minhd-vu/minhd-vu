d24e697961cb32c0bfe8e0befc8f03c7afc3ec0a
minhd-vu <minhd_vu@yahoo.com>
2023-04-02 04:43:58 -0400

    remove sensitive info, update postcss

--- website/src/components/Contact.tsx
+++ website/src/components/Contact.tsx
@@ -6,10 +6,10 @@ import { FaGithub, FaLinkedin, FaYoutube } from "react-icons/fa";
 export default function Contact() {
   return (
     <section id="contact" className="relative">
-      <div className="container lg:w-1/2 px-5 py-10 mx-auto">
+      <div className="container lg:w-1/2 2xl:w-1/3 px-5 py-10 mx-auto">
         <div className="bg-gray-900 rounded-lg overflow-hidden">
           <div className="bg-gray-800 relative flex lg:flex-row flex-col py-6 rounded shadow-md">
-            <div className="md:w-1/3 px-6">
+            <div className="md:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
@@ -20,15 +20,7 @@ export default function Contact() {
                 minhd_vu@yahoo.com
               </a>
             </div>
-
-            <div className="md:w-1/3 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                PHONE
-              </h2>
-              <p className="leading-relaxed">+1 (571) 319-9518</p>
-            </div>
-
-            <div className="md:w-1/3 px-6">
+            <div className="md:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 SOCIALS
               </h2>
