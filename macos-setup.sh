dc1c4a1c72b925b2f605e5abc5f560443cd21396
minhd-vu <minhd_vu@yahoo.com>
2024-02-12 12:18:28 -0500

    add hack nerd font install

--- macos-setup.sh
+++ macos-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install neovim asdf fzf
+brew tap homebrew/cask-fonts && brew install --cask font-hack-nerd-font
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
