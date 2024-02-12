3a12c112138986be201675bb33ef3d293d7265ab
Minh Vu <minhd_vu@yahoo.com>
2024-02-12 13:02:11 -0500

    add deps

--- .zshrc
+++ .zshrc
@@ -107,7 +107,7 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-alias ls="exa"
+alias ls="eza"
 alias cat="bat"
 
 export GOPATH=$HOME/go
--- macos-setup.sh
+++ macos-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install neovim asdf fzf bat eza
+brew install neovim asdf fzf bat eza lazygit
 brew tap homebrew/cask-fonts && brew install --cask font-hack-nerd-font
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
